3c57b668e32f59daef3773096a46765eb8f74a6f
ykrods <890082+ykrods@users.noreply.github.com>
2025-04-04 17:33:39 +0900

    disable minify

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
+    minify: false,
     sourcemap: true,
     outDir: "backend/dist",
   },
