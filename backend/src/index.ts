c8b3257dfa00428246b8cc7f83d86af8d03df815
ykrods <890082+ykrods@users.noreply.github.com>
2025-04-04 16:54:55 +0900

    using port given by render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -74,7 +74,7 @@ api.delete("/todos/:id", validateId, (req, res) => {
 
 app.use("/api", api);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
