f836767b11ecf69e81dc3e74067c56b4aba1d9ab
ykrods <890082+ykrods@users.noreply.github.com>
2025-04-04 13:49:43 +0900

    remove vite-plugin-spawn to make restart timing independent

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from "node:path";
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
-import { spawn } from "./vite-plugin-spawn.ts"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,6 +17,5 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     svelte(),
-    spawn("npm", "run", "dev", "-w", "backend", "--", "--clear-screen=false"),
   ],
 })
