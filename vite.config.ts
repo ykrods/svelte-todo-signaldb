1710d400313b978e9b9cda20c6e70ce61e91c9f7
ykrods <890082+ykrods@users.noreply.github.com>
2025-04-04 16:56:32 +0900

    output sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
+    sourcemap: true,
     outDir: "backend/dist",
   },
   resolve: {
