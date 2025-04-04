86ea4433526da9f1789b3fcc8562ccccc4a27d39
ykrods <890082+ykrods@users.noreply.github.com>
2025-04-04 17:48:18 +0900

    temporary fix to signaldb adapter

--- src/lib/Todos.svelte.ts
+++ src/lib/Todos.svelte.ts
@@ -2,11 +2,27 @@ import { Collection } from '@signaldb/core'
 import createLocalStorageAdapter from '@signaldb/localstorage'
 import svelteReactivityAdapter from '@signaldb/svelte'
 import syncManager from "./syncManager"
-
+import { createSubscriber } from "svelte/reactivity";
 
 const Todos = new Collection<Todo>({
   persistence: createLocalStorageAdapter('todos-svelte'),
-  reactivity: svelteReactivityAdapter,
+  reactivity: {
+    create() {
+      let _update;
+      const subscribe = createSubscriber((update) => {
+        _update = update;
+      });
+      return {
+        depend() {
+          subscribe()
+        },
+        notify() {
+          _update?.();
+        },
+      };
+    },
+    isInScope: () => !!$effect.tracking(),
+  }
 })
 
 syncManager.addCollection(Todos, {
